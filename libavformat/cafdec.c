FFmpeg/FFmpeg

commit c7579ad8e84c5aa28a0540604e8983cbde7ac37b

cafdec: replace deprecated get_strz()

libavformat/cafdec.c
@@ -198,8 +198,8 @@ static void read_info_chunk(AVFormatContext *s, int64_t size)
     for (i = 0; i < nb_entries; i++) {
         char key[32];
         char value[1024];
-        get_strz(pb, key, sizeof(key));
-        get_strz(pb, value, sizeof(value));
+        avio_get_str(pb, sizeof(key), key, sizeof(key));
+        avio_get_str(pb, sizeof(value), value, sizeof(value));
         av_dict_set(&s->metadata, key, value, 0);
     }
 }

rudkoLA/FFmpeg-DV-Vulkan

commit da78940b07162919e5e4570c2bf9375085601bef

cafdec: fix regression introduced in c7579ad8e84c5
Signed-off-by: Paul B Mahol <onemda@gmail.com>
Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

libavformat/cafdec.c
@@ -198,8 +198,8 @@ static void read_info_chunk(AVFormatContext *s, int64_t size)
     for (i = 0; i < nb_entries; i++) {
         char key[32];
         char value[1024];
-        avio_get_str(pb, sizeof(key), key, sizeof(key));
-        avio_get_str(pb, sizeof(value), value, sizeof(value));
+        avio_get_str(pb, INT_MAX, key, sizeof(key));
+        avio_get_str(pb, INT_MAX, value, sizeof(value));
         av_dict_set(&s->metadata, key, value, 0);
     }
 }