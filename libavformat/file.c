FFmpeg/FFmpeg

commit 4ed5ac50d3e4f921003ecf60985f78337400f354

file: return proper error on seek failures

libavformat/file.c
@@ -121,12 +121,18 @@ static int file_open(URLContext *h, const char *filename, int flags)
 static int64_t file_seek(URLContext *h, int64_t pos, int whence)
 {
     FileContext *c = h->priv_data;
+    int ret;
+
     if (whence == AVSEEK_SIZE) {
         struct stat st;
-        int ret = fstat(c->fd, &st);
+
+        ret = fstat(c->fd, &st);
         return ret < 0 ? AVERROR(errno) : st.st_size;
     }
-    return lseek(c->fd, pos, whence);
+
+    ret = lseek(c->fd, pos, whence);
+
+    return ret < 0 ? AVERROR(errno) : ret;
 }
 
 static int file_close(URLContext *h)

rudkoLA/FFmpeg-DV-Vulkan

commit 56a439bfb72c18cfb4bff9aef63a82a232eae978

file: set the return value type for lseek to off_t
fixes a bug introduced with 4ed5ac50d3e4f921003ecf60985f78337400f354

Signed-off-by: Jean First <jeanfirst@gmail.com>
Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

libavformat/file.c
@@ -135,7 +135,7 @@ static int file_open(URLContext *h, const char *filename, int flags)
 static int64_t file_seek(URLContext *h, int64_t pos, int whence)
 {
     FileContext *c = h->priv_data;
-    int ret;
+    off_t ret;
 
     if (whence == AVSEEK_SIZE) {
         struct stat st;