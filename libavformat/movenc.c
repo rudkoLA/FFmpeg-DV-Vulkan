FFmpeg/FFmpeg

commit b02493e47668e66757b72a7163476e590edfea3a

movenc: force video timebase to be 0.1ms precisse at least.
The timebases before where only guranteed to be 1/fps precisse
and could cause AV sync errors on low fps

Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

libavformat/movenc.c
@@ -3522,6 +3522,8 @@ static int mov_write_header(AVFormatContext *s)
                 track->height = track->tag>>24 == 'n' ? 486 : 576;
             }
             track->timescale = st->codec->time_base.den;
+            while(track->timescale < 10000)
+                track->timescale *= 2;
             if (track->mode == MODE_MOV && track->timescale > 100000)
                 av_log(s, AV_LOG_WARNING,
                        "WARNING codec timebase is very high. If duration is too long,\n"

tests/ref/lavf/mov
@@ -1,12 +1,12 @@
-0e5013f259148938ac771186308318db *./tests/data/lavf/lavf.mov
+fcbe7806047914d9751fd9053009df69 *./tests/data/lavf/lavf.mov
 367365 ./tests/data/lavf/lavf.mov
 ./tests/data/lavf/lavf.mov CRC=0xb2f59ab4
-9865100aa9c604d53cc5b4d804f94cdd *./tests/data/lavf/lavf.mov
+d3a85fc24ddd07e354645450073a30e3 *./tests/data/lavf/lavf.mov
 358455 ./tests/data/lavf/lavf.mov
 ./tests/data/lavf/lavf.mov CRC=0xb2f59ab4
 cbc587335946df1f08d838f48ef396f0 *./tests/data/lavf/lavf.mov
 377613 ./tests/data/lavf/lavf.mov
 ./tests/data/lavf/lavf.mov CRC=0xc882139b
-a42f977ed05f641d8aa5a69fc7291d5b *./tests/data/lavf/lavf.mov
+7c932d24837f46ef57d3e40a61331565 *./tests/data/lavf/lavf.mov
 357837 ./tests/data/lavf/lavf.mov
 ./tests/data/lavf/lavf.mov CRC=0xb2f59ab4

tests/ref/fate/vsynth1-avui
@@ -1,4 +1,4 @@
-6094fe77d3a4c49cae45a3de088bdb1a *tests/data/fate/vsynth1-avui.mov
+853dad3a1248614c6d61c2f9dc2a999c *tests/data/fate/vsynth1-avui.mov
 42624907 tests/data/fate/vsynth1-avui.mov
 c5ccac874dbf808e9088bc3107860042 *tests/data/fate/vsynth1-avui.out.rawvideo
 stddev:    0.00 PSNR:999.99 MAXDIFF:    0 bytes:  7603200/  7603200

tests/ref/fate/vsynth1-dnxhd-1080i
@@ -1,4 +1,4 @@
-fac7cbb7541f485ec3b1a7a24dbdca8f *tests/data/fate/vsynth1-dnxhd-1080i.mov
+9a4781b0a052d9efaafbaf8893db9632 *tests/data/fate/vsynth1-dnxhd-1080i.mov
 3031911 tests/data/fate/vsynth1-dnxhd-1080i.mov
 e55bf857297ba4d911a9d17a984b125d *tests/data/fate/vsynth1-dnxhd-1080i.out.rawvideo
 stddev:    6.29 PSNR: 32.15 MAXDIFF:   64 bytes:  7603200/   760320

tests/ref/fate/vsynth1-mpeg4
@@ -1,4 +1,4 @@
-1283a1904765c40b7602402867a86c4f *tests/data/fate/vsynth1-mpeg4.mp4
+a2acdf772bf7b7641079d8a03ea03ccf *tests/data/fate/vsynth1-mpeg4.mp4
 540024 tests/data/fate/vsynth1-mpeg4.mp4
 f80ec173d37f2f91add031e95579a220 *tests/data/fate/vsynth1-mpeg4.out.rawvideo
 stddev:    7.97 PSNR: 30.10 MAXDIFF:  105 bytes:  7603200/  7603200

tests/ref/fate/vsynth1-prores
@@ -1,4 +1,4 @@
-31159cc9d1693bc66ad7a3083c48bcc4 *tests/data/fate/vsynth1-prores.mov
+17a598a8cc6b899c79e3c69b9679e08d *tests/data/fate/vsynth1-prores.mov
 5022811 tests/data/fate/vsynth1-prores.mov
 a2e2d1d45341a94ff994d1d92629f778 *tests/data/fate/vsynth1-prores.out.rawvideo
 stddev:    2.47 PSNR: 40.27 MAXDIFF:   31 bytes:  7603200/  7603200

tests/ref/fate/vsynth1-prores_kostya
@@ -1,4 +1,4 @@
-e7a7e17a40df5d1296cea05d55bd5724 *tests/data/fate/vsynth1-prores_kostya.mov
+504b53c891d5bd917d5cf5190c312a20 *tests/data/fate/vsynth1-prores_kostya.mov
 3858901 tests/data/fate/vsynth1-prores_kostya.mov
 0a4153637d0cc0a88a8bcbf04cfaf8c6 *tests/data/fate/vsynth1-prores_kostya.out.rawvideo
 stddev:    3.17 PSNR: 38.09 MAXDIFF:   39 bytes:  7603200/  7603200

tests/ref/fate/vsynth1-qtrle
@@ -1,4 +1,4 @@
-9c6ee876369fceac1ff2478a0225bc9b *tests/data/fate/vsynth1-qtrle.mov
+f66efb8a1fd455ef31de540df9a81909 *tests/data/fate/vsynth1-qtrle.mov
 15263148 tests/data/fate/vsynth1-qtrle.mov
 93695a27c24a61105076ca7b1f010bbd *tests/data/fate/vsynth1-qtrle.out.rawvideo
 stddev:    3.42 PSNR: 37.44 MAXDIFF:   48 bytes:  7603200/  7603200

rudkoLA/FFmpeg-DV-Vulkan

commit 979c33f51df78d3b047a323aff0db7d719d8a722

movenc: fix timescale for timecode track
regression since b02493e476

Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

libavformat/movenc.c
@@ -3407,7 +3407,7 @@ static int mov_create_timecode_track(AVFormatContext *s, int index, int src_inde
     track->mode      = mov->mode;
     track->tag       = MKTAG('t','m','c','d');
     track->src_track = src_index;
-    track->timescale = src_st->codec->time_base.den;
+    track->timescale = mov->tracks[src_index].timescale;
     if (tc.flags & AV_TIMECODE_FLAG_DROPFRAME)
         track->timecode_flags |= MOV_TIMECODE_FLAG_DROPFRAME;
 

tests/ref/lavf/mov
@@ -1,7 +1,7 @@
 fcbe7806047914d9751fd9053009df69 *./tests/data/lavf/lavf.mov
 367365 ./tests/data/lavf/lavf.mov
 ./tests/data/lavf/lavf.mov CRC=0xb2f59ab4
-d3a85fc24ddd07e354645450073a30e3 *./tests/data/lavf/lavf.mov
+d023fdd2149f75ef231180b6b0b0a988 *./tests/data/lavf/lavf.mov
 358455 ./tests/data/lavf/lavf.mov
 ./tests/data/lavf/lavf.mov CRC=0xb2f59ab4
 cbc587335946df1f08d838f48ef396f0 *./tests/data/lavf/lavf.mov