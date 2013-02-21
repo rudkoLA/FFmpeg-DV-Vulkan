FFmpeg/FFmpeg

commit b040ffc84c5e35436590e0bcea6608a2ad31cf84

exr: display warning if multiple compression attributes are found
Signed-off-by: Paul B Mahol <onemda@gmail.com>

libavcodec/exr.c
@@ -403,7 +403,10 @@ static int decode_frame(AVCodecContext *avctx,
             if (!variable_buffer_data_size)
                 return AVERROR_INVALIDDATA;
 
-            s->compr = *buf;
+            if (s->compr == -1)
+                s->compr = *buf;
+            else
+                av_log(avctx, AV_LOG_WARNING, "Found more than one compression attribute\n");
 
             buf += variable_buffer_data_size;
             continue;

rudkoLA/FFmpeg-DV-Vulkan

commit 858924230eb9123d483c5f057e15073ab7a95d5b

exr: reset compression in decode_frame()
This fixes decoding of images if compression changes.
Regression since b040ffc84c5e.

Signed-off-by: Paul B Mahol <onemda@gmail.com>

libavcodec/exr.c
@@ -258,6 +258,7 @@ static int decode_frame(AVCodecContext *avctx,
     s->channel_offsets[2] = -1;
     s->channel_offsets[3] = -1;
     s->bits_per_color_id = -1;
+    s->compr = -1;
 
     if (buf_size < 10) {
         av_log(avctx, AV_LOG_ERROR, "Too short header to parse\n");
@@ -644,8 +645,6 @@ static av_cold int decode_init(AVCodecContext *avctx)
 
     avcodec_get_frame_defaults(&s->picture);
     avctx->coded_frame = &s->picture;
-
-    s->compr = -1;
 
     return 0;
 }