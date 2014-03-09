FFmpeg/FFmpeg

isom: add "NO16" FourCC
samples:

http://www.datafilehost.com/download-46d9a0fa.html

Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

libavformat/isom.c
@@ -86,6 +86,7 @@ const AVCodecTag ff_codec_movvideo_tags[] = {
     { CODEC_ID_RAWVIDEO, MKTAG('b', 'x', 'b', 'g') },
     { CODEC_ID_RAWVIDEO, MKTAG('b', 'x', 'r', 'g') },
     { CODEC_ID_RAWVIDEO, MKTAG('b', 'x', 'y', 'v') },
+    { CODEC_ID_RAWVIDEO, MKTAG('N', 'O', '1', '6') },
     { CODEC_ID_RAWVIDEO, MKTAG('D', 'V', 'O', 'O') }, /* Digital Voodoo SD 8 Bit */
     { CODEC_ID_RAWVIDEO, MKTAG('R', '4', '2', '0') }, /* Radius DV YUV PAL */
     { CODEC_ID_RAWVIDEO, MKTAG('R', '4', '1', '1') }, /* Radius DV YUV NTSC */

rudkoLA/FFmpeg-DV-Vulkan

libavformat/isom.c
@@ -81,6 +81,7 @@ const AVCodecTag ff_codec_movvideo_tags[] = {
     { AV_CODEC_ID_RAWVIDEO, MKTAG('A', 'B', 'G', 'R') },
     { AV_CODEC_ID_RAWVIDEO, MKTAG('b', '1', '6', 'g') },
     { AV_CODEC_ID_RAWVIDEO, MKTAG('b', '4', '8', 'r') },
+    { AV_CODEC_ID_RAWVIDEO, MKTAG('N', 'O', '1', '6') },
     { AV_CODEC_ID_RAWVIDEO, MKTAG('D', 'V', 'O', 'O') }, /* Digital Voodoo SD 8 Bit */
     { AV_CODEC_ID_RAWVIDEO, MKTAG('R', '4', '2', '0') }, /* Radius DV YUV PAL */
     { AV_CODEC_ID_RAWVIDEO, MKTAG('R', '4', '1', '1') }, /* Radius DV YUV NTSC */