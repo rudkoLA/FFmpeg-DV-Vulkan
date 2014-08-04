FFmpeg/FFmpeg

tiff: support reading gray+alpha at 8 bits
Signed-off-by: Vittorio Giovara <vittorio.giovara@gmail.com>

libavcodec/tiff.c
@@ -262,6 +262,9 @@ static int init_image(TiffContext *s, AVFrame *frame)
     case 161:
         s->avctx->pix_fmt = s->le ? AV_PIX_FMT_GRAY16LE : AV_PIX_FMT_GRAY16BE;
         break;
+    case 162:
+        s->avctx->pix_fmt = AV_PIX_FMT_GRAY8A;
+        break;
     case 324:
         s->avctx->pix_fmt = AV_PIX_FMT_RGBA;
         break;

rudkoLA/FFmpeg-DV-Vulkan

libavcodec/tiff.c
@@ -262,6 +262,9 @@ static int init_image(TiffContext *s, AVFrame *frame)
     case 161:
         s->avctx->pix_fmt = s->le ? AV_PIX_FMT_GRAY16LE : AV_PIX_FMT_GRAY16BE;
         break;
+    case 162:
+        s->avctx->pix_fmt = AV_PIX_FMT_YA8;
+        break;
     case 322:
         s->avctx->pix_fmt = s->le ? AV_PIX_FMT_YA16LE : AV_PIX_FMT_YA16BE;
         break;