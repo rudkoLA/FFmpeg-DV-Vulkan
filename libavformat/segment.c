FFmpeg/FFmpeg

commit 83a508cda5115c61b456aeb227bf770d61010961

lavf/segment: Fix memleak.
Reviewed-by: Stefano Sabatini
Reviewed-by: Ganesh Ajjanagadde

libavformat/segment.c
@@ -876,6 +876,7 @@ fail:
     av_opt_free(seg);
     av_freep(&seg->times);
     av_freep(&seg->frames);
+    av_freep(&seg->cur_entry.filename);
 
     cur = seg->segment_list_entries;
     while (cur) {

rudkoLA/FFmpeg-DV-Vulkan

commit cb06258fb9868b597c63dba00fe7e2f00b087038

avformat/segment: Do not free the filename twice
Bug introduced in 83a508cda5115c61b456aeb227bf770d61010961

Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

libavformat/segment.c
@@ -348,6 +348,7 @@ static int segment_end(AVFormatContext *s, int write_trailer, int is_last)
 
             /* append new element */
             memcpy(entry, &seg->cur_entry, sizeof(*entry));
+            entry->filename = av_strdup(entry->filename);
             if (!seg->segment_list_entries)
                 seg->segment_list_entries = seg->segment_list_entries_end = entry;
             else