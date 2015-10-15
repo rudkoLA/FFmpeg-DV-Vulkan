FFmpeg/FFmpeg

commit ad701326b43078b907a054c585d7f3308b7f6143

avformat/hls: open playlists immediately when AVDISCARD_ALL is dropped
Do not try to delay opening newly required playlists until a segment
switch. Applications expect that newly selected undiscarded streams are
available immediately, especially with alternative rendition streams
(selectable audio/subtitle tracks).

One might think that delaying variant stream switch until a segment
switch would allow a "seamless" switch without us having to download a
specific segment from two different variant playlists. However, that is
not the case, since the application would have to keep the previous
stream available (undiscarded) until the first packet of the newly
selected stream arrives, but by that time the demuxer would have already
downloaded the next segment of both variants.

Signed-off-by: Anssi Hannula <anssi.hannula@iki.fi>

libavformat/hls.c
@@ -164,7 +164,6 @@ typedef struct HLSContext {
     struct rendition **renditions;
 
     int cur_seq_no;
-    int end_of_segment;
     int first_packet;
     int64_t first_timestamp;
     int64_t cur_timestamp;
@@ -1053,7 +1052,6 @@ reload:
     v->input = NULL;
     v->cur_seq_no++;
 
-    c->end_of_segment = 1;
     c->cur_seq_no = v->cur_seq_no;
 
     goto restart;
@@ -1480,13 +1478,8 @@ static int hls_read_packet(AVFormatContext *s, AVPacket *pkt)
     HLSContext *c = s->priv_data;
     int ret, i, minplaylist = -1;
 
-    if (c->first_packet) {
-        recheck_discard_flags(s, 1);
-        c->first_packet = 0;
-    }
+    recheck_discard_flags(s, c->first_packet);
 
-start:
-    c->end_of_segment = 0;
     for (i = 0; i < c->n_playlists; i++) {
         struct playlist *pls = c->playlists[i];
         /* Make sure we've got one buffered packet from each open playlist
@@ -1551,10 +1544,7 @@ start:
             }
         }
     }
-    if (c->end_of_segment) {
-        if (recheck_discard_flags(s, 0))
-            goto start;
-    }
+
     /* If we got a packet, return it */
     if (minplaylist >= 0) {
         struct playlist *pls = c->playlists[minplaylist];

rudkoLA/FFmpeg-DV-Vulkan

commit 2e31a652ac04a34f26679906c08ed42f9a1beeca

avformat/hls: fix segment selection regression on track changes of live streams
Commit ad701326b43078b90 ("avformat/hls: open playlists immediately when
AVDISCARD_ALL is dropped") inadvertently caused first_packet to never be
cleared, causing select_cur_seq_no() to not use the specific code for
live streams.

In practice this means that when the user selects a different audio
track during live stream (i.e. non-VOD) playback, there may be some
additional delay as the code might select an incorrect segment at first,
and we have to wait for video to catch audio (if too late segment was
selected) or to download more following audio segments (if too early
segment was selected).

Fix that by restoring the zeroing of first_packet.

libavformat/hls.c
@@ -1602,6 +1602,7 @@ static int hls_read_packet(AVFormatContext *s, AVPacket *pkt)
     int ret, i, minplaylist = -1;
 
     recheck_discard_flags(s, c->first_packet);
+    c->first_packet = 0;
 
     for (i = 0; i < c->n_playlists; i++) {
         struct playlist *pls = c->playlists[i];