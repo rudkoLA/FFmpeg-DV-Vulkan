FFmpeg/FFmpeg

commit f3fdef108eb06b1e71b29152bf6822519e787efe

ape: Avoid undefined behaviour
Avoid the clang warning

"warning: shifting a negative signed value is undefined"

libavcodec/apedec.c
@@ -1305,7 +1305,7 @@ static void do_apply_filter(APEContext *ctx, int version, APEFilter *f,
             /* Update the adaption coefficients */
             absres = FFABS(res);
             if (absres)
-                *f->adaptcoeffs = ((res & (-1<<31)) ^ (-1<<30)) >>
+                *f->adaptcoeffs = ((res & ((~0UL) << 31)) ^ ((~0UL) << 30)) >>
                                   (25 + (absres <= f->avg*3) + (absres <= f->avg*4/3));
             else
                 *f->adaptcoeffs = 0;

rudkoLA/FFmpeg-DV-Vulkan

commit 3b91870da5527820afe2bc0e663808a0e72f68f9

ape: Unbreak adaptcoeffs computation
And simplify and explain the expression.

Fault introduced in f3fdef108eb06b1e71b29152bf6822519e787efe

libavcodec/apedec.c
@@ -1305,8 +1305,16 @@ static void do_apply_filter(APEContext *ctx, int version, APEFilter *f,
             /* Update the adaption coefficients */
             absres = FFABS(res);
             if (absres)
-                *f->adaptcoeffs = ((res & ((~0UL) << 31)) ^ ((~0UL) << 30)) >>
-                                  (25 + (absres <= f->avg*3) + (absres <= f->avg*4/3));
+                *f->adaptcoeffs = APESIGN(res) *
+                                  (8 << ((absres > f->avg * 3) + (absres > f->avg * 4 / 3)));
+                /* equivalent to the following code
+                    if (absres <= f->avg * 4 / 3)
+                        *f->adaptcoeffs = APESIGN(res) * 8;
+                    else if (absres <= f->avg * 3)
+                        *f->adaptcoeffs = APESIGN(res) * 16;
+                    else
+                        *f->adaptcoeffs = APESIGN(res) * 32;
+                */
             else
                 *f->adaptcoeffs = 0;
 