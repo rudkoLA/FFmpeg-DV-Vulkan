FFmpeg/FFmpeg

commit 736e2e2c30088d0c2a428a51372c78a0bfb7f356

avfilter/vf_shuffleframes: Assert that the case of an uninitialized ret does not occur
Fixes CID1258479

Reviewed-by: Paul B Mahol <onemda@gmail.com>
Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

libavfilter/vf_shuffleframes.c
@@ -18,6 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include "libavutil/avassert.h"
 #include "libavutil/avstring.h"
 #include "libavutil/common.h"
 #include "libavutil/internal.h"
@@ -90,9 +91,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
         s->pts[s->in_frames] = frame->pts;
         s->in_frames++;
         ret = 0;
-    }
-
-    if (s->in_frames == s->nb_frames) {
+    } else if (s->in_frames == s->nb_frames) {
         int n, x;
 
         for (n = 0; n < s->nb_frames; n++) {
@@ -109,7 +108,8 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
 
         for (n = 0; n < s->nb_frames; n++)
             av_frame_free(&s->frames[n]);
-    }
+    } else
+        av_assert0(0);
 
     return ret;
 }

rudkoLA/FFmpeg-DV-Vulkan

commit 74fbe8c0ae0ca2c37b0bbc2ee1ed3b83d186c342

avfilter/vf_shuffleframes: unbreak filter
Regression since 736e2e2c30088d0c2a428a51372c78a0bfb7f356.

Signed-off-by: Paul B Mahol <onemda@gmail.com>

libavfilter/vf_shuffleframes.c
@@ -84,14 +84,15 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
 {
     AVFilterContext    *ctx = inlink->dst;
     ShuffleFramesContext *s = ctx->priv;
-    int ret;
+    int ret = 0;
 
     if (s->in_frames < s->nb_frames) {
         s->frames[s->in_frames] = frame;
         s->pts[s->in_frames] = frame->pts;
         s->in_frames++;
-        ret = 0;
-    } else if (s->in_frames == s->nb_frames) {
+    }
+
+    if (s->in_frames == s->nb_frames) {
         int n, x;
 
         for (n = 0; n < s->nb_frames; n++) {
@@ -108,8 +109,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
 
         for (n = 0; n < s->nb_frames; n++)
             av_frame_free(&s->frames[n]);
-    } else
-        av_assert0(0);
+    }
 
     return ret;
 }
@@ -117,6 +117,11 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
 static av_cold void uninit(AVFilterContext *ctx)
 {
     ShuffleFramesContext *s = ctx->priv;
+
+    while (s->in_frames > 0) {
+        s->in_frames--;
+        av_frame_free(&s->frames[s->in_frames]);
+    }
 
     av_freep(&s->frames);
     av_freep(&s->map);