FFmpeg/FFmpeg

commit a22c6a4796ca1f2cbee6784262515da876fbec22

avcodec/encode: remove usage of av_dup_packet()
Reviewed-by: wm4 <nfxjfg@googlemail.com>
Signed-off-by: James Almer <jamrial@gmail.com>

libavcodec/encode.c
@@ -222,10 +222,12 @@ int attribute_align_arg avcodec_encode_audio2(AVCodecContext *avctx,
             }
             avpkt->buf      = user_pkt.buf;
             avpkt->data     = user_pkt.data;
-        } else {
-            if (av_dup_packet(avpkt) < 0) {
-                ret = AVERROR(ENOMEM);
-            }
+        } else if (!avpkt->buf) {
+            AVPacket tmp = { 0 };
+            ret = av_packet_ref(&tmp, avpkt);
+            if (ret < 0)
+                return ret;
+            *avpkt = tmp;
         }
     }
 
@@ -318,10 +320,12 @@ int attribute_align_arg avcodec_encode_video2(AVCodecContext *avctx,
             }
             avpkt->buf      = user_pkt.buf;
             avpkt->data     = user_pkt.data;
-        } else {
-            if (av_dup_packet(avpkt) < 0) {
-                ret = AVERROR(ENOMEM);
-            }
+        } else if (!avpkt->buf) {
+            AVPacket tmp = { 0 };
+            ret = av_packet_ref(&tmp, avpkt);
+            if (ret < 0)
+                return ret;
+            *avpkt = tmp;
         }
     }
 

rudkoLA/FFmpeg-DV-Vulkan

commit 61b417bc2285a95e249cd78afd19a73e58a5dd89

avcodec/encode: do proper cleanup on failure
Fixes the last remaining memleaks introduced by a22c6a4796ca1f2cbee6784262515da876fbec22.

Signed-off-by: James Almer <jamrial@gmail.com>

libavcodec/encode.c
@@ -225,9 +225,9 @@ int attribute_align_arg avcodec_encode_audio2(AVCodecContext *avctx,
         } else if (!avpkt->buf) {
             AVPacket tmp = { 0 };
             ret = av_packet_ref(&tmp, avpkt);
-            if (ret < 0)
-                return ret;
             av_packet_unref(avpkt);
+            if (ret < 0)
+                goto end;
             *avpkt = tmp;
         }
     }
@@ -324,9 +324,9 @@ int attribute_align_arg avcodec_encode_video2(AVCodecContext *avctx,
         } else if (!avpkt->buf) {
             AVPacket tmp = { 0 };
             ret = av_packet_ref(&tmp, avpkt);
+            av_packet_unref(avpkt);
             if (ret < 0)
                 return ret;
-            av_packet_unref(avpkt);
             *avpkt = tmp;
         }
     }