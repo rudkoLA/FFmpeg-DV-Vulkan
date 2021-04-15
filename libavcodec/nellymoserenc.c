FFmpeg/FFmpeg

commit 0a56bfa71f751a2b25da8d060a019c1c75ca9d7b

avcodec/nellymoserenc: Cleanup generically on init failure
Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

libavcodec/nellymoserenc.c
@@ -170,12 +170,10 @@ static av_cold int encode_init(AVCodecContext *avctx)
     ff_af_queue_init(avctx, &s->afq);
     s->avctx = avctx;
     if ((ret = ff_mdct_init(&s->mdct_ctx, 8, 0, 32768.0)) < 0)
-        goto error;
+        return ret;
     s->fdsp = avpriv_float_dsp_alloc(avctx->flags & AV_CODEC_FLAG_BITEXACT);
-    if (!s->fdsp) {
-        ret = AVERROR(ENOMEM);
-        goto error;
-    }
+    if (!s->fdsp)
+        return AVERROR(ENOMEM);
 
     /* Generate overlap window */
     ff_init_ff_sine_windows(7);
@@ -195,16 +193,11 @@ static av_cold int encode_init(AVCodecContext *avctx)
     if (s->avctx->trellis) {
         s->opt  = av_malloc(NELLY_BANDS * OPT_SIZE * sizeof(float  ));
         s->path = av_malloc(NELLY_BANDS * OPT_SIZE * sizeof(uint8_t));
-        if (!s->opt || !s->path) {
-            ret = AVERROR(ENOMEM);
-            goto error;
-        }
+        if (!s->opt || !s->path)
+            return AVERROR(ENOMEM);
     }
 
     return 0;
-error:
-    encode_end(avctx);
-    return ret;
 }
 
 #define find_best(val, table, LUT, LUT_add, LUT_size) \
@@ -431,4 +424,5 @@ AVCodec ff_nellymoser_encoder = {
     .capabilities   = AV_CODEC_CAP_SMALL_LAST_FRAME | AV_CODEC_CAP_DELAY,
     .sample_fmts    = (const enum AVSampleFormat[]){ AV_SAMPLE_FMT_FLT,
                                                      AV_SAMPLE_FMT_NONE },
+    .caps_internal  = FF_CODEC_CAP_INIT_CLEANUP,
 };

rudkoLA/FFmpeg-DV-Vulkan

commit 340cbcdc81b7fb258031806ede03db2fc7b20ed3

avcodec/nellymoserenc: Fix segfault when using unsupported channels/rate
NellyMoserEncodeContext.avctx is only set in init after these checks,
yet it is used by encode_end().
This is a regression since 0a56bfa71f751a2b25da8d060a019c1c75ca9d7b.

Reviewed-by: Michael Niedermayer <michael@niedermayer.cc>
Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

libavcodec/nellymoserenc.c
@@ -138,10 +138,8 @@ static av_cold int encode_end(AVCodecContext *avctx)
 
     ff_mdct_end(&s->mdct_ctx);
 
-    if (s->avctx->trellis) {
-        av_freep(&s->opt);
-        av_freep(&s->path);
-    }
+    av_freep(&s->opt);
+    av_freep(&s->path);
     ff_af_queue_close(&s->afq);
     av_freep(&s->fdsp);
 