FFmpeg/FFmpeg

commit 753930bc7300dd595c4bab51c5a70d1da9083da4

doc: make apidoc output independent of SRC_PATH
Previously it included the SRC_PATH in every title.

Reviewed-by: Michael Niedermayer <michael@niedermayer.cc>
Signed-off-by: Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>

doc/Makefile
@@ -124,11 +124,12 @@ $(DOCS) doc/doxy/html: | doc/
 $(DOC_EXAMPLES:%$(EXESUF)=%.o): | doc/examples
 OBJDIRS += doc/examples
 
-DOXY_INPUT      = $(addprefix $(SRC_PATH)/, $(INSTHEADERS) $(DOC_EXAMPLES:%$(EXESUF)=%.c) $(LIB_EXAMPLES:%$(EXESUF)=%.c))
+DOXY_INPUT      = $(INSTHEADERS) $(DOC_EXAMPLES:%$(EXESUF)=%.c) $(LIB_EXAMPLES:%$(EXESUF)=%.c)
+DOXY_INPUT_DEPS = $(addprefix $(SRC_PATH)/, $(DOXY_INPUT))
 
 doc/doxy/html: TAG = DOXY
-doc/doxy/html: $(SRC_PATH)/doc/Doxyfile $(SRC_PATH)/doc/doxy-wrapper.sh $(DOXY_INPUT)
-	$(M)$(SRC_PATH)/doc/doxy-wrapper.sh $(SRC_PATH) $< $(DOXYGEN) $(DOXY_INPUT)
+doc/doxy/html: $(SRC_PATH)/doc/Doxyfile $(SRC_PATH)/doc/doxy-wrapper.sh $(DOXY_INPUT_DEPS)
+	$(M)OUT_DIR=$$PWD/doc/doxy; cd $(SRC_PATH); ./doc/doxy-wrapper.sh $$OUT_DIR $< $(DOXYGEN) $(DOXY_INPUT);
 
 install-doc: install-html install-man
 

doc/doxy-wrapper.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-SRC_PATH="${1}"
+OUT_DIR="${1}"
 DOXYFILE="${2}"
 DOXYGEN="${3}"
 
 shift 3
 
-if [ -e "$SRC_PATH/VERSION" ]; then
-    VERSION=`cat "$SRC_PATH/VERSION"`
+if [ -e "VERSION" ]; then
+    VERSION=`cat "VERSION"`
 else
-    VERSION=`cd "$SRC_PATH"; git describe`
+    VERSION=`git describe`
 fi
 
 $DOXYGEN - <<EOF
 @INCLUDE        = ${DOXYFILE}
 INPUT           = $@
-EXAMPLE_PATH    = ${SRC_PATH}/doc/examples
 HTML_TIMESTAMP  = NO
 PROJECT_NUMBER  = $VERSION
+OUTPUT_DIRECTORY = $OUT_DIR
 EOF

rudkoLA/FFmpeg-DV-Vulkan

commit 1ab69cf1dc17c135965effd8929a4581f672d905

doc: fix generating doxy with out-of-tree builds
Broken in 753930bc7300dd595c4bab51c5a70d1da9083da4, as the path to
Doxyfile passed to doxy-wrapper.sh is relative to the build dir, while
the recipe cd's to the source dir before invoking the wrapper.

doc/Makefile
@@ -102,7 +102,7 @@ DOXY_INPUT_DEPS = $(addprefix $(SRC_PATH)/, $(DOXY_INPUT)) ffbuild/config.mak
 
 doc/doxy/html: TAG = DOXY
 doc/doxy/html: $(SRC_PATH)/doc/Doxyfile $(SRC_PATH)/doc/doxy-wrapper.sh $(DOXY_INPUT_DEPS)
-	$(M)OUT_DIR=$$PWD/doc/doxy; cd $(SRC_PATH); ./doc/doxy-wrapper.sh $$OUT_DIR $< $(DOXYGEN) $(DOXY_INPUT);
+	$(M)$(SRC_PATH)/doc/doxy-wrapper.sh $$PWD/doc/doxy $(SRC_PATH) doc/Doxyfile $(DOXYGEN) $(DOXY_INPUT);
 
 install-doc: install-html install-man
 

doc/doxy-wrapper.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 OUT_DIR="${1}"
-DOXYFILE="${2}"
-DOXYGEN="${3}"
+SRC_DIR="${2}"
+DOXYFILE="${3}"
+DOXYGEN="${4}"
 
-shift 3
+shift 4
+
+cd ${SRC_DIR}
 
 if [ -e "VERSION" ]; then
     VERSION=`cat "VERSION"`