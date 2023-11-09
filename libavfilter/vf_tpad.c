FFmpeg/FFmpeg

commit 86b252ea9dee18006910e30646ad1067f2d1323f

avfilter/vf_tpad: accept hardware frames in clone-only mode
When no drawing is to be performed, tpad can work fine with
hardware frames, so advertise this in the query_formats
callback and ensure the drawing context is never initialised
when just cloning frames.

Reviewed-by: Thilo Borgmann <thilo.borgmann@mail.de>
Reviewed-by: Niklas Haas <git@haasn.dev>

libavfilter/vf_tpad.c
@@ -71,7 +71,12 @@ AVFILTER_DEFINE_CLASS(tpad);
 
 static int query_formats(AVFilterContext *ctx)
 {
-    return ff_set_common_formats(ctx, ff_draw_supported_pixel_formats(0));
+    TPadContext *s = ctx->priv;
+    if ((s->stop_mode == MODE_ADD && s->pad_stop != 0) ||
+        (s->start_mode == MODE_ADD && s->pad_start != 0))
+        return ff_set_common_formats(ctx, ff_draw_supported_pixel_formats(0));
+
+    return ff_set_common_formats(ctx, ff_all_formats(AVMEDIA_TYPE_VIDEO));
 }
 
 static int activate(AVFilterContext *ctx)
@@ -190,8 +195,11 @@ static int config_input(AVFilterLink *inlink)
     AVFilterContext *ctx = inlink->dst;
     TPadContext *s = ctx->priv;
 
-    ff_draw_init(&s->draw, inlink->format, 0);
-    ff_draw_color(&s->draw, &s->color, s->rgba_color);
+    if ((s->stop_mode == MODE_ADD && s->pad_stop != 0) ||
+        (s->start_mode == MODE_ADD && s->pad_start != 0)) {
+        ff_draw_init(&s->draw, inlink->format, 0);
+        ff_draw_color(&s->draw, &s->color, s->rgba_color);
+    }
 
     if (s->start_duration)
         s->pad_start = av_rescale_q(s->start_duration, inlink->frame_rate, av_inv_q(AV_TIME_BASE_Q));

rudkoLA/FFmpeg-DV-Vulkan

commit 5799e247d232c588cce04be1143989f4575b2879

avfilter/vf_tpad: fix check for drawing initialization
The check if drawing needs to be initialized and supported formats
should be drawable ones was flawed, as pad_stop/pad_start is only
populated from stop_duration/start_duration after these checks.

To fix that, check the _duration variants as well and for better
readability and maintainability break the check out into its own
helper.

Fixes a regression from 86b252ea9dee18006910e30646ad1067f2d1323f
Fix #10621

Signed-off-by: Anton Khirnov <anton@khirnov.net>

libavfilter/vf_tpad.c
@@ -70,11 +70,17 @@ static const AVOption tpad_options[] = {
 
 AVFILTER_DEFINE_CLASS(tpad);
 
+static int needs_drawing(const TPadContext *s) {
+    return (
+        (s->stop_mode  == MODE_ADD && (s->pad_stop  != 0 || s->stop_duration  != 0)) ||
+        (s->start_mode == MODE_ADD && (s->pad_start != 0 || s->start_duration != 0))
+    );
+}
+
 static int query_formats(AVFilterContext *ctx)
 {
     TPadContext *s = ctx->priv;
-    if ((s->stop_mode == MODE_ADD && s->pad_stop != 0) ||
-        (s->start_mode == MODE_ADD && s->pad_start != 0))
+    if (needs_drawing(s))
         return ff_set_common_formats(ctx, ff_draw_supported_pixel_formats(0));
 
     return ff_set_common_formats(ctx, ff_all_formats(AVMEDIA_TYPE_VIDEO));
@@ -196,8 +202,7 @@ static int config_input(AVFilterLink *inlink)
     AVFilterContext *ctx = inlink->dst;
     TPadContext *s = ctx->priv;
 
-    if ((s->stop_mode == MODE_ADD && s->pad_stop != 0) ||
-        (s->start_mode == MODE_ADD && s->pad_start != 0)) {
+    if (needs_drawing(s)) {
         ff_draw_init(&s->draw, inlink->format, 0);
         ff_draw_color(&s->draw, &s->color, s->rgba_color);
     }