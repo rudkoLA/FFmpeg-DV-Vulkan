FFmpeg/FFmpeg

commit 0df18f29ae218f30b3ecff7c930176b8ebee56aa

avfilter/af_afir: Only keep DSP stuff in header
Only the AudioFIRDSPContext and the functions for its initialization
are needed outside of lavfi/af_afir.c.
Also rename the header to af_afirdsp.h to reflect the change.

Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

libavfilter/af_afir.c
@@ -30,8 +30,11 @@
 #include "libavutil/channel_layout.h"
 #include "libavutil/common.h"
 #include "libavutil/float_dsp.h"
+#include "libavutil/frame.h"
 #include "libavutil/intreadwrite.h"
+#include "libavutil/log.h"
 #include "libavutil/opt.h"
+#include "libavutil/rational.h"
 #include "libavutil/xga_font_data.h"
 
 #include "audio.h"
@@ -39,7 +42,73 @@
 #include "filters.h"
 #include "formats.h"
 #include "internal.h"
-#include "af_afir.h"
+#include "af_afirdsp.h"
+
+typedef struct AudioFIRSegment {
+    int nb_partitions;
+    int part_size;
+    int block_size;
+    int fft_length;
+    int coeff_size;
+    int input_size;
+    int input_offset;
+
+    int *output_offset;
+    int *part_index;
+
+    AVFrame *sumin;
+    AVFrame *sumout;
+    AVFrame *blockin;
+    AVFrame *blockout;
+    AVFrame *buffer;
+    AVFrame *coeff;
+    AVFrame *input;
+    AVFrame *output;
+
+    AVTXContext **tx, **itx;
+    av_tx_fn tx_fn, itx_fn;
+} AudioFIRSegment;
+
+typedef struct AudioFIRContext {
+    const AVClass *class;
+
+    float wet_gain;
+    float dry_gain;
+    float length;
+    int gtype;
+    float ir_gain;
+    int ir_format;
+    float max_ir_len;
+    int response;
+    int w, h;
+    AVRational frame_rate;
+    int ir_channel;
+    int minp;
+    int maxp;
+    int nb_irs;
+    int selir;
+
+    float gain;
+
+    int eof_coeffs[32];
+    int have_coeffs;
+    int nb_taps;
+    int nb_channels;
+    int nb_coef_channels;
+    int one2many;
+
+    AudioFIRSegment seg[1024];
+    int nb_segments;
+
+    AVFrame *in;
+    AVFrame *ir[32];
+    AVFrame *video;
+    int min_part_size;
+    int64_t pts;
+
+    AudioFIRDSPContext afirdsp;
+    AVFloatDSPContext *fdsp;
+} AudioFIRContext;
 
 static void fcmul_add_c(float *sum, const float *t, const float *c, ptrdiff_t len)
 {

libavfilter/af_afir.h
@@ -1,108 +1,0 @@
-/*
- * Copyright (c) 2017 Paul B Mahol
- *
- * This file is part of FFmpeg.
- *
- * FFmpeg is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * FFmpeg is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
-
-#ifndef AVFILTER_AFIR_H
-#define AVFILTER_AFIR_H
-
-#include <stddef.h>
-#include <stdint.h>
-
-#include "libavutil/tx.h"
-#include "libavutil/float_dsp.h"
-#include "libavutil/frame.h"
-#include "libavutil/log.h"
-#include "libavutil/rational.h"
-
-typedef struct AudioFIRSegment {
-    int nb_partitions;
-    int part_size;
-    int block_size;
-    int fft_length;
-    int coeff_size;
-    int input_size;
-    int input_offset;
-
-    int *output_offset;
-    int *part_index;
-
-    AVFrame *sumin;
-    AVFrame *sumout;
-    AVFrame *blockin;
-    AVFrame *blockout;
-    AVFrame *buffer;
-    AVFrame *coeff;
-    AVFrame *input;
-    AVFrame *output;
-
-    AVTXContext **tx, **itx;
-    av_tx_fn tx_fn, itx_fn;
-} AudioFIRSegment;
-
-typedef struct AudioFIRDSPContext {
-    void (*fcmul_add)(float *sum, const float *t, const float *c,
-                      ptrdiff_t len);
-} AudioFIRDSPContext;
-
-typedef struct AudioFIRContext {
-    const AVClass *class;
-
-    float wet_gain;
-    float dry_gain;
-    float length;
-    int gtype;
-    float ir_gain;
-    int ir_format;
-    float max_ir_len;
-    int response;
-    int w, h;
-    AVRational frame_rate;
-    int ir_channel;
-    int minp;
-    int maxp;
-    int nb_irs;
-    int selir;
-
-    float gain;
-
-    int eof_coeffs[32];
-    int have_coeffs;
-    int nb_taps;
-    int nb_channels;
-    int nb_coef_channels;
-    int one2many;
-
-    AudioFIRSegment seg[1024];
-    int nb_segments;
-
-    AVFrame *in;
-    AVFrame *ir[32];
-    AVFrame *video;
-    int min_part_size;
-    int64_t pts;
-
-    AudioFIRDSPContext afirdsp;
-    AVFloatDSPContext *fdsp;
-
-} AudioFIRContext;
-
-void ff_afir_init(AudioFIRDSPContext *s);
-void ff_afir_init_x86(AudioFIRDSPContext *s);
-
-#endif /* AVFILTER_AFIR_H */

tests/checkasm/af_afir.c
@@ -21,7 +21,7 @@
 #include <float.h>
 #include <stdint.h>
 
-#include "libavfilter/af_afir.h"
+#include "libavfilter/af_afirdsp.h"
 #include "libavutil/internal.h"
 #include "libavutil/mem_internal.h"
 #include "checkasm.h"

libavfilter/af_afirdsp.h
@@ -1,4 +1,6 @@
 /*
+ * Copyright (c) 2017 Paul B Mahol
+ *
  * This file is part of FFmpeg.
  *
  * FFmpeg is free software; you can redistribute it and/or
@@ -16,25 +18,17 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "config.h"
-#include "libavutil/attributes.h"
-#include "libavutil/cpu.h"
-#include "libavutil/x86/cpu.h"
-#include "libavfilter/af_afirdsp.h"
+#ifndef AVFILTER_AFIRDSP_H
+#define AVFILTER_AFIRDSP_H
 
-void ff_fcmul_add_sse3(float *sum, const float *t, const float *c,
-                       ptrdiff_t len);
-void ff_fcmul_add_avx(float *sum, const float *t, const float *c,
+#include <stddef.h>
+
+typedef struct AudioFIRDSPContext {
+    void (*fcmul_add)(float *sum, const float *t, const float *c,
                       ptrdiff_t len);
+} AudioFIRDSPContext;
 
-av_cold void ff_afir_init_x86(AudioFIRDSPContext *s)
-{
-    int cpu_flags = av_get_cpu_flags();
+void ff_afir_init(AudioFIRDSPContext *s);
+void ff_afir_init_x86(AudioFIRDSPContext *s);
 
-    if (EXTERNAL_SSE3(cpu_flags)) {
-        s->fcmul_add = ff_fcmul_add_sse3;
-    }
-    if (EXTERNAL_AVX_FAST(cpu_flags)) {
-        s->fcmul_add = ff_fcmul_add_avx;
-    }
-}
+#endif /* AVFILTER_AFIRDSP_H */

rudkoLA/FFmpeg-DV-Vulkan

commit 231f8181d98bd4b3e6d805df955e66433c4b9ccf

avfilter/af_afir: Merge header into af_afir.c
Done in 0df18f29ae218f30b3ecff7c930176b8ebee56aa,
accidentally undone in 163e737c1793eeea9c2df15298253ffc04906afe.

Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

libavfilter/af_afir.c
@@ -34,14 +34,93 @@
 #include "libavutil/frame.h"
 #include "libavutil/log.h"
 #include "libavutil/opt.h"
+#include "libavutil/rational.h"
 
 #include "audio.h"
 #include "avfilter.h"
 #include "filters.h"
 #include "formats.h"
 #include "internal.h"
-#include "af_afir.h"
 #include "af_afirdsp.h"
+
+#define MAX_IR_STREAMS 32
+
+typedef struct AudioFIRSegment {
+    int nb_partitions;
+    int part_size;
+    int block_size;
+    int fft_length;
+    int coeff_size;
+    int input_size;
+    int input_offset;
+
+    int *output_offset;
+    int *part_index;
+
+    AVFrame *sumin;
+    AVFrame *sumout;
+    AVFrame *blockout;
+    AVFrame *tempin;
+    AVFrame *tempout;
+    AVFrame *buffer;
+    AVFrame *coeff;
+    AVFrame *input;
+    AVFrame *output;
+
+    AVTXContext **ctx, **tx, **itx;
+    av_tx_fn ctx_fn, tx_fn, itx_fn;
+} AudioFIRSegment;
+
+typedef struct AudioFIRContext {
+    const AVClass *class;
+
+    float wet_gain;
+    float dry_gain;
+    float length;
+    int gtype;
+    float ir_norm;
+    float ir_link;
+    float ir_gain;
+    int ir_format;
+    int ir_load;
+    float max_ir_len;
+    int response;
+    int w, h;
+    AVRational frame_rate;
+    int ir_channel;
+    int minp;
+    int maxp;
+    int nb_irs;
+    int prev_selir;
+    int selir;
+    int precision;
+    int format;
+
+    int eof_coeffs[MAX_IR_STREAMS];
+    int have_coeffs[MAX_IR_STREAMS];
+    int nb_taps[MAX_IR_STREAMS];
+    int nb_segments[MAX_IR_STREAMS];
+    int max_offset[MAX_IR_STREAMS];
+    int nb_channels;
+    int one2many;
+    int prev_is_disabled;
+    int *loading;
+    double *ch_gain;
+
+    AudioFIRSegment seg[MAX_IR_STREAMS][1024];
+
+    AVFrame *in;
+    AVFrame *xfade[2];
+    AVFrame *fadein[2];
+    AVFrame *ir[MAX_IR_STREAMS];
+    AVFrame *norm_ir[MAX_IR_STREAMS];
+    int min_part_size;
+    int max_part_size;
+    int64_t pts;
+
+    AudioFIRDSPContext afirdsp;
+    AVFloatDSPContext *fdsp;
+} AudioFIRContext;
 
 #define DEPTH 32
 #include "afir_template.c"

libavfilter/af_afir.h
@@ -1,110 +1,0 @@
-/*
- * Copyright (c) 2017 Paul B Mahol
- *
- * This file is part of FFmpeg.
- *
- * FFmpeg is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * FFmpeg is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
-
-#ifndef AVFILTER_AFIR_H
-#define AVFILTER_AFIR_H
-
-#include "libavutil/float_dsp.h"
-#include "libavutil/frame.h"
-#include "libavutil/rational.h"
-#include "libavutil/tx.h"
-#include "avfilter.h"
-#include "af_afirdsp.h"
-
-#define MAX_IR_STREAMS 32
-
-typedef struct AudioFIRSegment {
-    int nb_partitions;
-    int part_size;
-    int block_size;
-    int fft_length;
-    int coeff_size;
-    int input_size;
-    int input_offset;
-
-    int *output_offset;
-    int *part_index;
-
-    AVFrame *sumin;
-    AVFrame *sumout;
-    AVFrame *blockout;
-    AVFrame *tempin;
-    AVFrame *tempout;
-    AVFrame *buffer;
-    AVFrame *coeff;
-    AVFrame *input;
-    AVFrame *output;
-
-    AVTXContext **ctx, **tx, **itx;
-    av_tx_fn ctx_fn, tx_fn, itx_fn;
-} AudioFIRSegment;
-
-typedef struct AudioFIRContext {
-    const AVClass *class;
-
-    float wet_gain;
-    float dry_gain;
-    float length;
-    int gtype;
-    float ir_norm;
-    float ir_link;
-    float ir_gain;
-    int ir_format;
-    int ir_load;
-    float max_ir_len;
-    int response;
-    int w, h;
-    AVRational frame_rate;
-    int ir_channel;
-    int minp;
-    int maxp;
-    int nb_irs;
-    int prev_selir;
-    int selir;
-    int precision;
-    int format;
-
-    int eof_coeffs[MAX_IR_STREAMS];
-    int have_coeffs[MAX_IR_STREAMS];
-    int nb_taps[MAX_IR_STREAMS];
-    int nb_segments[MAX_IR_STREAMS];
-    int max_offset[MAX_IR_STREAMS];
-    int nb_channels;
-    int one2many;
-    int prev_is_disabled;
-    int *loading;
-    double *ch_gain;
-
-    AudioFIRSegment seg[MAX_IR_STREAMS][1024];
-
-    AVFrame *in;
-    AVFrame *xfade[2];
-    AVFrame *fadein[2];
-    AVFrame *ir[MAX_IR_STREAMS];
-    AVFrame *norm_ir[MAX_IR_STREAMS];
-    int min_part_size;
-    int max_part_size;
-    int64_t pts;
-
-    AudioFIRDSPContext afirdsp;
-    AVFloatDSPContext *fdsp;
-} AudioFIRContext;
-
-#endif /* AVFILTER_AFIR_H */