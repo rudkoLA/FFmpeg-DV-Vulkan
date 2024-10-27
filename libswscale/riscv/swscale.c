ramiropolla/ffmpeg

swscale/range_convert: call arch-specific init functions from main init function
This commit also fixes the issue that the call to ff_sws_init_range_convert()
from sws_init_swscale() was not setting up the arch-specific optimizations.

libswscale/riscv/swscale.c
@@ -26,9 +26,11 @@ void ff_range_chr_to_jpeg_16_rvv(int16_t *, int16_t *, int);
 void ff_range_lum_from_jpeg_16_rvv(int16_t *, int);
 void ff_range_chr_from_jpeg_16_rvv(int16_t *, int16_t *, int);
 
-av_cold static void ff_sws_init_range_convert_riscv(SwsContext *c, int flags)
+av_cold void ff_sws_init_range_convert_riscv(SwsContext *c)
 {
 #if HAVE_RVV
+    int flags = av_get_cpu_flags();
+
     static const struct {
         void (*lum)(int16_t *, int);
         void (*chr)(int16_t *, int16_t *, int);
@@ -67,9 +69,9 @@ RVV_INPUT(rgba32);
 
 av_cold void ff_sws_init_swscale_riscv(SwsContext *c)
 {
+#if HAVE_RVV
     int flags = av_get_cpu_flags();
 
-#if HAVE_RVV
     if ((flags & AV_CPU_FLAG_RVV_I32) && (flags & AV_CPU_FLAG_RVB)) {
         switch (c->srcFormat) {
             case AV_PIX_FMT_ABGR:
@@ -122,5 +124,4 @@ av_cold void ff_sws_init_swscale_riscv(SwsContext *c)
         }
     }
 #endif
-    ff_sws_init_range_convert_riscv(c, flags);
 }

libswscale/swscale_internal.h
@@ -697,6 +697,7 @@ void ff_updateMMXDitherTables(SwsContext *c, int dstY);
 av_cold void ff_sws_init_range_convert(SwsContext *c);
 av_cold void ff_sws_init_range_convert_aarch64(SwsContext *c);
 av_cold void ff_sws_init_range_convert_loongarch(SwsContext *c);
+av_cold void ff_sws_init_range_convert_riscv(SwsContext *c);
 av_cold void ff_sws_init_range_convert_x86(SwsContext *c);
 
 SwsFunc ff_yuv2rgb_init_x86(SwsContext *c);

rudkoLA/FFmpeg-DV-Vulkan

libswscale/riscv/swscale.c
@@ -26,9 +26,11 @@ void ff_range_chr_to_jpeg_16_rvv(int16_t *, int16_t *, int);
 void ff_range_lum_from_jpeg_16_rvv(int16_t *, int);
 void ff_range_chr_from_jpeg_16_rvv(int16_t *, int16_t *, int);
 
-av_cold static void ff_sws_init_range_convert_riscv(SwsInternal *c, int flags)
+av_cold void ff_sws_init_range_convert_riscv(SwsInternal *c)
 {
 #if HAVE_RVV
+    int flags = av_get_cpu_flags();
+
     static const struct {
         void (*lum)(int16_t *, int);
         void (*chr)(int16_t *, int16_t *, int);
@@ -67,9 +69,9 @@ RVV_INPUT(rgba32);
 
 av_cold void ff_sws_init_swscale_riscv(SwsInternal *c)
 {
+#if HAVE_RVV
     int flags = av_get_cpu_flags();
 
-#if HAVE_RVV
     if ((flags & AV_CPU_FLAG_RVV_I32) && (flags & AV_CPU_FLAG_RVB)) {
         switch (c->srcFormat) {
             case AV_PIX_FMT_ABGR:
@@ -122,5 +124,4 @@ av_cold void ff_sws_init_swscale_riscv(SwsInternal *c)
         }
     }
 #endif
-    ff_sws_init_range_convert_riscv(c, flags);
 }

libswscale/swscale_internal.h
@@ -719,6 +719,7 @@ void ff_update_palette(SwsInternal *c, const uint32_t *pal);
 av_cold void ff_sws_init_range_convert(SwsInternal *c);
 av_cold void ff_sws_init_range_convert_aarch64(SwsInternal *c);
 av_cold void ff_sws_init_range_convert_loongarch(SwsInternal *c);
+av_cold void ff_sws_init_range_convert_riscv(SwsInternal *c);
 av_cold void ff_sws_init_range_convert_x86(SwsInternal *c);
 
 SwsFunc ff_yuv2rgb_init_x86(SwsInternal *c);