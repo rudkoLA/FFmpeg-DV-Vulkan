FFmpeg/FFmpeg

commit 19e9a203b7b8e613840b055cdf68303a4fb84581

lavu/dict: add AV_DICT_DEDUP
This is useful when multiple metadata inputs may set the same value
(e.g. both a container-specific header and an ID3 tag).

Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

libavutil/dict.c
@@ -101,6 +101,17 @@ int av_dict_set(AVDictionary **pm, const char *key, const char *value,
     }
     if (!(flags & AV_DICT_MULTIKEY)) {
         tag = av_dict_get(m, key, NULL, flags);
+    } else if (flags & AV_DICT_DEDUP) {
+        while ((tag = av_dict_get(m, key, tag, flags))) {
+            if ((!value && !tag->value) ||
+                (value && tag->value && !strcmp(value, tag->value))) {
+                if (flags & AV_DICT_DONT_STRDUP_KEY)
+                    av_free((void*)key);
+                if (flags & AV_DICT_DONT_STRDUP_VAL)
+                    av_free((void*)value);
+                return 0;
+            }
+        }
     }
     if (flags & AV_DICT_DONT_STRDUP_KEY)
         copy_key = (void *)key;

libavutil/dict.h
@@ -82,6 +82,7 @@
 #define AV_DICT_APPEND         32   /**< If the entry already exists, append to it.  Note that no
                                          delimiter is added, the strings are simply concatenated. */
 #define AV_DICT_MULTIKEY       64   /**< Allow to store several equal keys in the dictionary */
+#define AV_DICT_DEDUP         128   /**< If inserting a value that already exists for a key, do nothing. Only relevant with AV_DICT_MULTIKEY. */
 /**
  * @}
  */

rudkoLA/FFmpeg-DV-Vulkan

commit 0a3af9f94b9746b864ceacb477a29a81240f4a02

avutil/dict: fix memleak in av_dict_set()
Regression since 19e9a203b7b8e613840b055cdf68303a4fb84581.

Signed-off-by: James Almer <jamrial@gmail.com>

libavutil/dict.c
@@ -99,27 +99,28 @@ int av_dict_set(AVDictionary **pm, const char *key, const char *value,
         err = AVERROR(EINVAL);
         goto err_out;
     }
+    if (flags & AV_DICT_DONT_STRDUP_KEY)
+        copy_key = (void *)key;
+    else
+        copy_key = av_strdup(key);
+    if (!copy_key || (value && !copy_value))
+        goto enomem;
+
     if (!(flags & AV_DICT_MULTIKEY)) {
         tag = av_dict_get(m, key, NULL, flags);
     } else if (flags & AV_DICT_DEDUP) {
         while ((tag = av_dict_get(m, key, tag, flags))) {
             if ((!value && !tag->value) ||
                 (value && tag->value && !strcmp(value, tag->value))) {
-                if (flags & AV_DICT_DONT_STRDUP_KEY)
-                    av_free((void*)key);
-                if (flags & AV_DICT_DONT_STRDUP_VAL)
-                    av_free((void*)value);
+                av_free(copy_key);
+                av_free(copy_value);
                 return 0;
             }
         }
     }
-    if (flags & AV_DICT_DONT_STRDUP_KEY)
-        copy_key = (void *)key;
-    else
-        copy_key = av_strdup(key);
     if (!m)
         m = *pm = av_mallocz(sizeof(*m));
-    if (!m || !copy_key || (value && !copy_value))
+    if (!m)
         goto enomem;
 
     if (tag) {