FFmpeg/FFmpeg

commit e29016a9de8cf9a15569bdcea6e68c8e9ba2f299

avformat/subfile: clip seek offset
Fixes: signed integer overflow: 9223372036854737920 + 1649410 cannot be
represented in type 'int64_t'

Fixes OSS-Fuzz: 410100610

Signed-off-by: Kacper Michajłow <kasper93@gmail.com>

libavformat/subfile.c
@@ -128,13 +128,13 @@ static int64_t subfile_seek(URLContext *h, int64_t pos, int whence)
     case AVSEEK_SIZE:
         return end - c->start;
     case SEEK_SET:
-        new_pos = c->start + pos;
+        new_pos = c->start + av_clip(pos, 0, end - c->start);
         break;
     case SEEK_CUR:
-        new_pos = c->pos + pos;
+        new_pos = c->pos + av_clip(pos, -(c->pos - c->start), end - c->pos);
         break;
     case SEEK_END:
-        new_pos = end + pos;
+        new_pos = end + av_clip(pos, -(end - c->start), 0);
         break;
     default:
         av_assert0(0);

rudkoLA/FFmpeg-DV-Vulkan

commit cd080fac40c36a4eac27dd7ab61e6cf0d804afa1

avformat/subfile: Initialize end on all cases
Fixes: use of uninitialized variable
Regression since: e29016a9de8cf9a15569bdcea6e68c8e9ba2f299

Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

libavformat/subfile.c
@@ -118,11 +118,9 @@ static int64_t subfile_seek(URLContext *h, int64_t pos, int whence)
     int64_t new_pos, end;
     int ret;
 
-    if (whence == AVSEEK_SIZE || whence == SEEK_END) {
         end = c->end;
         if (end == INT64_MAX && (end = ffurl_seek(c->h, 0, AVSEEK_SIZE)) < 0)
             return end;
-    }
 
     switch (whence) {
     case AVSEEK_SIZE: