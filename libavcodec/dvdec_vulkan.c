d5b6893dee9fd95a1de36d76942d1727814368a6
antondep <antondep007@gmail.com>
2026-04-09 19:55:47 +0000

    Initialize Vulkan hwaccel for DV and fix initialization stability

--- libavcodec/dvdec_vulkan.c
+++ libavcodec/dvdec_vulkan.c
@@ -35,13 +35,15 @@
  * DV decoder
  */
 
-
+#define FF_INTERNAL_FIELDS
+#define INDENT_shd INDENT(0)
 #include "config.h"
 #include "config_components.h"
 #include <errno.h>
 
 #include "libavutil/avassert.h"
 #include "libavutil/internal.h"
+#include "libavcodec/avcodec.h"
 #include "libavutil/mem_internal.h"
 #include "libavutil/thread.h"
 
@@ -65,34 +67,82 @@
 #include "libavutil/hwcontext_vulkan.h"
 #include "libavutil/vulkan.h"
 
+typedef struct DVSubContext
+{
+    FFVulkanDecodeContext vk_dec;
+    FFVulkanShader shd;
+    struct FFVulkanPipeline *pl;
+} DVSubContext;
+
 static int dv_vulkan_decode_init(AVCodecContext *avctx)
 {
-    AVHWDeviceContext *device_ctx;
-    AVVulkanDeviceContext *vk_dev;
+    // 1. Try to get private data directly from avctx
+    DVSubContext *s = avctx->internal->hwaccel_priv_data;
 
-    av_log(avctx, AV_LOG_INFO, "VULKAN_DEBUG: DV Vulkan init requested\n");
+    // 2. If that's null, check the internal pointer
+    if (!s && avctx->internal)
+        s = avctx->internal->hwaccel_priv_data;
 
-    if (!avctx->hw_device_ctx) {
-        av_log(avctx, AV_LOG_ERROR, "VULKAN_DEBUG: Missing hw_device_ctx\n");
+    // 3. If it's STILL null, we have to stop before we crash
+    if (!s)
+    {
+        av_log(avctx, AV_LOG_ERROR, "VULKAN_DEBUG: No hwaccel_priv_data found!\n");
         return AVERROR(EINVAL);
     }
 
-    device_ctx = (AVHWDeviceContext *)avctx->hw_device_ctx->data;
-    if (!device_ctx || device_ctx->type != AV_HWDEVICE_TYPE_VULKAN) {
-        av_log(avctx, AV_LOG_ERROR, "VULKAN_DEBUG: Invalid Vulkan device context\n");
+    // Now proceed with your checks
+    if (!avctx->hw_device_ctx)
+    {
+        av_log(avctx, AV_LOG_ERROR, "VULKAN_DEBUG: No hw_device_ctx found!\n");
         return AVERROR(EINVAL);
     }
 
-    vk_dev = device_ctx->hwctx;
-    if (!vk_dev) {
-        av_log(avctx, AV_LOG_ERROR, "VULKAN_DEBUG: Missing AVVulkanDeviceContext\n");
+    int err;
+
+    // 1. Try to initialize. If it fails, log it but let's see why.
+    err = ff_vk_decode_init(avctx);
+    if (err < 0)
+    {
+        av_log(avctx, AV_LOG_ERROR, "VULKAN_DEBUG: ff_vk_decode_init failed (%d)\n", err);
+        return err;
+    }
+
+    // 2. The Pointer Path Fix
+    // If shared_ctx is null, we might need to access the context from the device_ctx instead
+    FFVulkanContext *vk_ctx;
+    if (s->vk_dec.shared_ctx)
+    {
+        vk_ctx = &s->vk_dec.shared_ctx->s;
+    }
+    else
+    {
+        // Fallback: try to get it from the hardware device context
+        AVHWDeviceContext *device_ctx = (AVHWDeviceContext *)avctx->hw_device_ctx->data;
+        AVVulkanDeviceContext *vk_dev = device_ctx->hwctx;
+        // In some versions, you can use the device context to init the shader
+        av_log(avctx, AV_LOG_ERROR, "VULKAN_DEBUG: shared_ctx is NULL, shader init might fail\n");
         return AVERROR(EINVAL);
     }
 
-    av_log(avctx, AV_LOG_INFO, "VULKAN_DEBUG: Found Vulkan Device Context\n");
-    av_log(avctx, AV_LOG_ERROR, "VULKAN_DEBUG: DV Vulkan hwaccel is not yet implemented and exits gracefully\n");
+    // 3. Shader Init
+    err = ff_vk_shader_init(&s->vk_dec.shared_ctx->s, &s->shd, "dv_compute",
+                            VK_SHADER_STAGE_COMPUTE_BIT,
+                            NULL, 0, "main", 8, 8, 1);
 
-    return AVERROR(ENOSYS);
+    if (err >= 0)
+    {
+        av_log(avctx, AV_LOG_INFO, "VULKAN_DEBUG: Shader initialized successfully!\n");
+    }
+
+    // 3. GLSL Definition
+    FFVulkanShader *shd = &s->shd;
+    GLSLC(0, layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;);
+    GLSLC(0, void main() {);
+    GLSLC(0,     uint idx = gl_GlobalInvocationID.x;);
+    GLSLC(0, });
+
+    // 4. Link
+    return ff_vk_shader_link(vk_ctx, &s->shd, NULL, 0, NULL);
 }
 
 static int dv_vulkan_start_frame(AVCodecContext *avctx,
@@ -116,16 +166,17 @@ static int dv_vulkan_decode_slice(AVCodecContext *avctx, const uint8_t *data, ui
 }
 
 const FFHWAccel ff_dv_vulkan_hwaccel = {
-    .p.name      = "dv_vulkan",
-    .p.type      = AVMEDIA_TYPE_VIDEO,
-    .p.id        = AV_CODEC_ID_DVVIDEO,
-    .p.pix_fmt   = AV_PIX_FMT_VULKAN,
-    .start_frame   = dv_vulkan_start_frame,
-    .end_frame     = dv_vulkan_end_frame,
-    .decode_slice  = dv_vulkan_decode_slice,
-    .init          = dv_vulkan_decode_init,
-    .uninit        = ff_vk_decode_uninit,
-    .frame_params  = ff_vk_frame_params,
-    .priv_data_size = sizeof(FFVulkanDecodeContext),
-    .caps_internal  = HWACCEL_CAP_THREAD_SAFE,
+    .p.name = "dv_vulkan",
+    .p.type = AVMEDIA_TYPE_VIDEO,
+    .p.id = AV_CODEC_ID_DVVIDEO,
+    .p.pix_fmt = AV_PIX_FMT_VULKAN,
+    .start_frame = dv_vulkan_start_frame,
+    .end_frame = dv_vulkan_end_frame,
+    .decode_slice = dv_vulkan_decode_slice,
+    .init = dv_vulkan_decode_init,
+    .uninit = ff_vk_decode_uninit,
+    .frame_params = ff_vk_frame_params,
+    .priv_data_size = sizeof(DVSubContext),
+    .caps_internal = HWACCEL_CAP_THREAD_SAFE,
+    .priv_data_size = 0,
 };
